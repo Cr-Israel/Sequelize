98f9d1502a82dfdda7cbc6a73f6389da585073ed
Carlos Israel <98050243+Cr-Israel@users.noreply.github.com>
2023-03-16 10:22:47 -0300

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('database name', 'user', 'password', {
-    host: '',
+const sequelize = new Sequelize('database', 'username', 'password', {
+    host: 'localhost',
     dialect: 'mysql'
 });
 
